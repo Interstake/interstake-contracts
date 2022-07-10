266eac539571f0e09e3a1fa31a2369f7534cd958
Jakub Bogucki <software-solutions@tuta.io>
2022-07-10 14:42:54 +0200

    yield-generator: Simplify api

--- contracts/publica-yield-generator/src/msg.rs
+++ contracts/publica-yield-generator/src/msg.rs
@@ -1,24 +1,30 @@
-use crate::state::Config;
-use cosmwasm_std::Uint128;
-use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use schemars::JsonSchema;
+
+use cosmwasm_std::Decimal;
+
+use crate::state::Config;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
+    /// Multisig contract that is allowed to perform admin operations
     pub owner: String,
+    /// Denom in which contract stakes
+    pub denom: String,
+    /// Address of validator
     pub staking_addr: String,
-    pub reward_rate: Uint128,
-    pub reward_token: String,
+    /// Commission of Intrastake team
+    pub team_commision: Option<Decimal>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     UpdateConfig {
-        owner: String,
-        staking_addr: String,
-        reward_rate: Uint128,
-        reward_token: String,
+        owner: Option<String>,
+        denom: Option<String>,
+        staking_addr: Option<String>,
+        team_commision: Option<Decimal>,
     },
     Delegate {},
     Undelegate {},
@@ -35,5 +41,4 @@ pub enum QueryMsg {
 pub struct InfoResponse {
     pub config: Config,
     pub last_payment_block: u64,
-    pub balance: Uint128,
 }
--- contracts/publica-yield-generator/src/state.rs
+++ contracts/publica-yield-generator/src/state.rs
@@ -1,14 +1,15 @@
-use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{Addr, Decimal};
+use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     pub owner: Addr,
+    pub denom: String,
     pub staking_addr: Addr,
-    pub reward_rate: Uint128,
-    pub reward_token: Addr,
+    pub team_commision: Decimal,
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
